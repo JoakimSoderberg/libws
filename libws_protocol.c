07dad724d6b0d224afeb8f319ade6405354e3866
Joakim Söderberg <joakim.soderberg@gmail.com>
2013-09-10 22:18:42 +0200

    Added unpacking of header.

--- libws_protocol.c
+++ libws_protocol.c
@@ -1,28 +1,85 @@
 
 #include "libws_config.h"
+#include "libws_log.h"
 #include "libws_types.h"
 #include "libws_protocol.h"
 
-int ws_read_header(const unsigned char *data, size_t len)
+int ws_unpack_header(ws_header_t *h, size_t *header_len, 
+					const unsigned char *b, size_t len)
 {
-	assert(data);
+	assert(b);
+	assert(h);
+	assert(header_len);
+
+	*header_len = 0;
 
 	if (len < WS_MIN_HEADER_SIZE)
 	{
-		return -1;
+		goto fail;
 	}
 
-	// TODO: Unpack header
+	// First byte.
+	h->fin		= (b[0] >> 7) & 0x1;
+	h->rsv1		= (b[0] >> 6) & 0x1;
+	h->rsv2		= (b[0] >> 5) & 0x1;
+	h->rsv3		= (b[0] >> 4) & 0x1;
+	h->opcode	= (b[0] >> 3) & 0xF;
+
+	// Second byte.
+	h->mask_bit = (b[1] >> 7) & 0x1;
+	h->payload_len = (b[1] & 0x7F);
+
+	*header_len = 2;
+
+	// Get extended payload size if any (2 or 8 bytes).
+	if (h->payload_len == 126)
+	{
+		if (len < (*header_len + 2))
+		{
+			goto fail;
+		}
+		else
+		{
+			// 2 byte payload length.
+			uint16_t *size_ptr = (uint16_t *)&b[2];
+			h->payload_len = (uint64_t)ntohs(*size_ptr);
+			*header_len += 2;
+		}
+	}
+	else if (h->payload_len == 127)
+	{
+		if (len < (*header_len + 8))
+		{
+			goto fail;
+		}
+		else
+		{
+			// 8 byte payload length.
+			uint64_t *size_ptr = (uint64_t *)&b[2];
+			// TODO: Use ntohll here!
+			h->payload_len = (uint64_t)(*size_ptr);
+			*header_len += 8;
+		}
+	}
+
+	// Read masking key.
+	{
+		uint32_t *mask_ptr = (uint32_t *)&b[*header_len];
+		h->mask = *mask_ptr;
+		*header_len += 4;
+	}
 
 	return 0;
+fail:
+	LIBWS_LOG(LIBWS_ERR, "Not enough data to unpack header");
+	return -1;
 }
 
-void ws_pack_header_first_byte(ws_header_t *h, uint8_t *b)
+static void ws_pack_header_first_byte(ws_header_t *h, uint8_t *b)
 {
 	assert(h);
 	assert(b);
 
-	// TODO: Pack the header in network byte order.
 	//  7 6 5 4 3 2 1 0
 	// +-+-+-+-+-------+
 	// |F|R|R|R| opcode|
@@ -38,11 +95,11 @@ void ws_pack_header_first_byte(ws_header_t *h, uint8_t *b)
 	b[0] |= (h->opcode  & 0xF);
 }
 
-void ws_pack_header_rest(ws_header_t *h, uint8_t *b, size_t len, size_t *header_len)
+static void ws_pack_header_rest(ws_header_t *h, uint8_t *b, size_t len, size_t *header_len)
 {
 	assert(h);
 	assert(b);
-	assert(len >= WS_MAX_HEADER_SIZE);
+	assert(len >= WS_HDR_MAX_SIZE);
 	assert(h->payload_len <= WS_MAX_PAYLOAD_LEN);
 	assert(header_len);
 
@@ -79,10 +136,11 @@ void ws_pack_header_rest(ws_header_t *h, uint8_t *b, size_t len, size_t *header_
 	else if (h->payload_len == 126)
 	{
 		// 2 byte extra payload length.
+		uint16_t *size_ptr = (uint16_t *)&b[2];
 		*header_len += 2;
 
 		b[1] = 126;
-		*((uint16_t *)&b[2]) = htons((uint16_t)h->payload_len);
+		*size_ptr = htons((uint16_t)h->payload_len);
 	}
 	else
 	{
@@ -94,6 +152,13 @@ void ws_pack_header_rest(ws_header_t *h, uint8_t *b, size_t len, size_t *header_
 		//b[2] = ...
 		// TODO: Pack uint64_t in network byte order. Use htonll (not portable)? 
 	}
+
+	if (h->mask_bit)
+	{
+		uint32_t *mask_ptr = (uint32_t *)&b[*header_len];
+		*mask_ptr = htonl(h->mask);
+		*header_len += 4;
+	}
 }
 
 void ws_pack_header(ws_header_t *h, uint8_t *b, size_t len, size_t *header_len)
@@ -101,5 +166,4 @@ void ws_pack_header(ws_header_t *h, uint8_t *b, size_t len, size_t *header_len)
 	ws_pack_header_first_byte(h, b);
 	ws_pack_header_rest(h, b, len, header_len);
 }
-
 
--- libws_protocol.h
+++ libws_protocol.h
@@ -3,29 +3,6 @@
 
 #include <inttypes.h>
 
-// 
-// Websocket protocol RFC 6455: http://tools.ietf.org/html/rfc6455
-//
-//      0                   1                   2                   3
-//      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-//     +-+-+-+-+-------+-+-------------+-------------------------------+
-//     |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
-//     |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
-//     |N|V|V|V|       |S|             |   (if payload len==126/127)   |
-//     | |1|2|3|       |K|             |                               |
-//     +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
-//     |     Extended payload length continued, if payload len == 127  |
-//     + - - - - - - - - - - - - - - - +-------------------------------+
-//     |                               |Masking-key, if MASK set to 1  |
-//     +-------------------------------+-------------------------------+
-//     | Masking-key (continued)       |          Payload Data         |
-//     +-------------------------------- - - - - - - - - - - - - - - - +
-//     :                     Payload Data continued ...                :
-//     + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
-//     |                     Payload Data continued ...                |
-//     +---------------------------------------------------------------+
-//
-
 typedef enum ws_opcode_e
 {
 	WS_OPCODE_CONTINUATION = 0x0,
@@ -62,108 +39,55 @@ typedef enum ws_opcode_e
 #define WS_HDR_MIN_SIZE WS_HDR_BASE_SIZE
 #define WS_HDR_MAX_SIZE (WS_HDR_BASE_SIZE + WS_HDR_PAYLOAD_LEN_SIZE + WS_HDR_MASK_SIZE)
 
+///
+/// Websocket header structure.
+///
+/// 
+/// Websocket protocol RFC 6455: http://tools.ietf.org/html/rfc6455
+///
+///      0                   1                   2                   3
+///      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+///     +-+-+-+-+-------+-+-------------+-------------------------------+
+///     |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+///     |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+///     |N|V|V|V|       |S|             |   (if payload len==126/127)   |
+///     | |1|2|3|       |K|             |                               |
+///     +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+///     |     Extended payload length continued, if payload len == 127  |
+///     + - - - - - - - - - - - - - - - +-------------------------------+
+///     |                               |Masking-key, if MASK set to 1  |
+///     +-------------------------------+-------------------------------+
+///     | Masking-key (continued)       |          Payload Data         |
+///     +-------------------------------- - - - - - - - - - - - - - - - +
+///     :                     Payload Data continued ...                :
+///     + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+///     |                     Payload Data continued ...                |
+///     +---------------------------------------------------------------+
+///
 typedef struct ws_header_s
 {
-	uint8_t fin;
-	uint8_t rsv1;
-	uint8_t rsv2;
-	uint8_t rsv3;
-	ws_opcode_t opcode;
-	uint8_t mask_bit;
-	uint64_t payload_len;
-	uint32_t mask;
+	uint8_t fin;			///< Final frame bit.
+	uint8_t rsv1;			///< Reserved bit 1 for extensions.
+	uint8_t rsv2;			///< Reserved bit 2 for extensions. 
+	uint8_t rsv3;			///< Reserved bit 3 for extensions.
+	ws_opcode_t opcode;		///< Operation code.
+	uint8_t mask_bit;		///< If this frame is masked this bit is set.
+	uint64_t payload_len;	///< Length of the payload.
+	uint32_t mask;			///< Masking key for the payload.
 } ws_header_t;
 
-LIBWS_INLINE void ws_pack_header_first_byte(ws_header_t *h, uint8_t *b)
-{
-	assert(h);
-	assert(b);
+///
+/// Packs a websocket header struct into a network byte order
+/// octet stream.
+///
+/// @param[in]	h 			A pointer to the header to pack.
+/// @param[in]	b 			The byte buffer to pack the header in. 
+///							Must be at least #WS_HDR_MAX_SIZE big.
+/// @param[in]	len 		The size of the buffer.
+/// @param[out] header_len	Will contain the final size of the packed header.
+///
+void ws_pack_header(ws_header_t *h, uint8_t *b, size_t len, size_t *header_len);
 
-	// TODO: Pack the header in network byte order.
-	//  7 6 5 4 3 2 1 0
-	// +-+-+-+-+-------+
-	// |F|R|R|R| opcode|
-	// |I|S|S|S|  (4)  |
-	// |N|V|V|V|       |
-	// | |1|2|3|       |
-	// +-+-+-+-+-------+
-	b[0] = 0;
-	b[0] |= ((!!h->fin)  << 7);
-	b[0] |= ((!!h->rsv1) << 6);
-	b[0] |= ((!!h->rsv2) << 5);
-	b[0] |= ((!!h->rsv3) << 4);
-	b[0] |= (h->opcode  & 0xF);
-}
-
-LIBWS_INLINE void ws_pack_header_rest(ws_header_t *h, uint8_t *b, size_t len, size_t *header_len)
-{
-	assert(h);
-	assert(b);
-	assert(len >= WS_HDR_MAX_SIZE);
-	assert(h->payload_len <= WS_MAX_PAYLOAD_LEN);
-	assert(header_len);
-
-	// +----------------+---------------+-------------------------------+
-	// |    Byte 0      |     Byte 1    |     Byte 2+                   |
-	// +----------------+---------------+-------------------------------+
-	//					 7 6 5 4 3 2 1 0
-    //                  +-+-------------+-------------------------------+
-    //                  |M| Payload len |    Extended payload length    |
-    //                  |A|     (7)     |             (16/64)           |
-    //                  |S|             |   (if payload len==126/127)   |
-    //                  |K|             |                               |
-    //  +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
-    //  |     Extended payload length continued, if payload len == 127  |
-    //  + - - - - - - - - - - - - - - - +-------------------------------+
-    //  |                               |Masking-key, if MASK set to 1  |
-    //  +-------------------------------+-------------------------------+
-    //  | Masking-key (continued)       |
-    //  +--------------------------------
-
-	b[1] = 0;
-
-	// Masking bit. 
-	// (This MUST be set for a client according to RFC).
-	b[1] |= ((!!h->mask_bit) << 7); 
-
-	*header_len = 2;
-
-	if (h->payload_len < 126)
-	{
-		// Use 1 byte for payload len.
-		b[1] |= h->payload_len;
-	}
-	else if (h->payload_len == 126)
-	{
-		// 2 byte extra payload length.
-		*header_len += 2;
-
-		b[1] = 126;
-		*((uint16_t *)&b[2]) = htons((uint16_t)h->payload_len);
-	}
-	else
-	{
-		// 8 byte extra payload length.
-		*header_len += 8;
-
-		b[1] = 127;
-
-		//b[2] = ...
-		// TODO: Pack uint64_t in network byte order. Use htonll (not portable)? 
-	}
-
-	if (h->mask_bit)
-	{
-		*((uint32_t *)&b[*header_len]) = htonl(h->mask);
-		*header_len += 4;
-	}
-}
-
-LIBWS_INLINE void ws_pack_header(ws_header_t *h, uint8_t *b, size_t len, size_t *header_len)
-{
-	ws_pack_header_first_byte(h, b);
-	ws_pack_header_rest(h, b, len, header_len);
-}
 
 
 #endif // __LIBWS_PROTOCOL_H__
