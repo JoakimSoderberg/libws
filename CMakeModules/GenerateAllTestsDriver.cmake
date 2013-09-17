836fea76acbdd71e34737e00973d9e81f650ed1d
Joakim Soderberg <joakim.soderberg@gmail.com>
2013-09-17 16:35:39 +0000

    Made the all tests driver generator less complex.

--- CMakeModules/GenerateAllTestsDriver.cmake
+++ CMakeModules/GenerateAllTestsDriver.cmake
@@ -1,25 +1,29 @@
-#cmake_minimum_required (VERSION 2.6) 
 
+function(generate_run_all_tests_driver _DRIVER_NAME _DRIVER_SRC_NAME _EXTRA_SOURCE_FILES)
 
-function(generate_run_all_tests_driver _DRIVER_NAME _FUNCTION_NAMES _EXTRA_SOURCE_FILES)
 	set(FORWARD_DECLARATIONS "")
 	set(FUNCTION_CALLS "")
 
-	message("${FORWARD_DECLARATIONS}")
+	# Get the function names by removing the extension of the files.
+	# TEST_helloworld.c -> int TEST_helloworld(int argc, char *argv[])
+	set(_FUNCTION_NAMES)
+	foreach (test ${_EXTRA_SOURCE_FILES})
+		get_filename_component(TName ${test} NAME_WE)
+		list(APPEND _FUNCTION_NAMES ${TName})
+	endforeach()
 
 	foreach (func ${_FUNCTION_NAMES})
 		set(FORWARD_DECLARATIONS 
-			"${FORWARD_DECLARATIONS}int ${func}(int argc, char *[]);\n")
+			"${FORWARD_DECLARATIONS}int ${func}(int argc, char *argv[]);\n")
 
 		set(FUNCTION_CALLS 
 			"${FUNCTION_CALLS}\t${func}(argc, argv);\n")
 	endforeach()
 
 	configure_file("${CMAKE_MODULE_PATH}/run_all_tests_driver.c.in"
-		"${CMAKE_BINARY_DIR}/run_all_tests_driver.c")
-	
-	add_executable(${_DRIVER_NAME} "${CMAKE_BINARY_DIR}/run_all_tests_driver.c"
-				 	${_EXTRA_SOURCE_FILES})
+				"${_DRIVER_SRC_NAME}")
+
+	set(${_DRIVER_NAME} "${_DRIVER_SRC_NAME};${_EXTRA_SOURCE_FILES}" PARENT_SCOPE)
 
 endfunction() # generate_run_all_tests_driver
 
