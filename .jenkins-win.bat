b36a50ce04994182ff2a6ca15118e678ba1c9dab
Joakim Söderberg <joakim.soderberg@gmail.com>
2014-03-06 22:35:38 +0100

    Update the jenkins build to build Libevent.

--- .jenkins-win.bat
+++ .jenkins-win.bat
@@ -1,7 +1,15 @@
+cd deps
+cd libevent
+md build
+cd build
+cmake -DEVENT__DISABLE_TESTS=ON -DEVENT__DISABLE_REGRESS=ON -DEVENT__DISABLE_BENCHMARK=ON .. 
+cd ..
+cd ..
+cd ..
 
 md build
 cd build
-cmake -DLIBWS_WITH_AUTOBAHN=ON ..
+cmake -DLibevent_DIR=deps/libevent/build/  -DLIBWS_WITH_AUTOBAHN=ON ..
 cmake --build .
 ctest
 bin\Debug\autobahntest.exe --config ..\test\autobahn\libws.cfg
